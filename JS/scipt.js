6c2ae81b2ce299a3b10ea91493fbf9c492d39cdd
Infolix <158166326+Infolix@users.noreply.github.com>
2025-01-26 19:51:34 +0000

    Add files via upload

---
+++ JS/scipt.js
@@ -0,0 +1,32 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+  document.querySelector('.menu ul').classList.toggle('active');
+});
+
+new Swiper('.card', {
+    loop: true,
+    spaceBetween: 30,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },
+    }
+  });
